63a930b34d66875a48d6101d34ea83022b440caf
SakthiVijayS <124193377+SakthiVijayS@users.noreply.github.com>
2023-02-11 09:28:22 +0530

    Add files via upload

---
+++ src/data/User.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+import javax.swing.ImageIcon;
+/**
+ *
+ * @author shakt
+ */
+public class User {
+    String firstname;
+    String lastname;
+    int    age;
+    int    teleno;
+    String email;
+    String address;
+    String city;
+    String country;
+    String major;
+    ImageIcon pp;
+     
+    
+    
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTeleno() {
+        return teleno;
+    }
+
+    public void setTeleno(int teleno) {
+        this.teleno = teleno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public ImageIcon getPp() {
+        return pp;
+    }
+
+    public void setPp(ImageIcon pp) {
+        this.pp = pp;
+    }
+    
+}
